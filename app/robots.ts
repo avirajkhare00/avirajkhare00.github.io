3b9e15aa2064430d9e2121aa8982867702557b2d
avirajkhare00 <avirajkhare00@gmail.com>
2024-11-27 09:38:31 +0530

    [Aviraj] Changes in robots and sitemap files

--- app/robots.ts
+++ app/robots.ts
@@ -1,12 +1,16 @@
 import { baseUrl } from 'app/sitemap'
+import { MetadataRoute } from 'next'
 
-export default function robots() {
+export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
       {
         userAgent: '*',
+        allow: '/',
+        disallow: ['/api/', '/_next/'],
       },
     ],
     sitemap: `${baseUrl}/sitemap.xml`,
+    host: baseUrl,
   }
 }
