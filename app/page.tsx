9dfee53ecd036dc722b097e8a7cfc1a25f45a9f5
avirajkhare00 <avirajkhare00@gmail.com>
2024-11-27 11:01:07 +0530

    [Aviraj] Changed title of blog

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,11 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        avirajkhare00
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`I love to work on Site Reliability and Infrastructure.
+          Find my projects on GitHub.`}
       </p>
       <div className="my-8">
         <BlogPosts />
